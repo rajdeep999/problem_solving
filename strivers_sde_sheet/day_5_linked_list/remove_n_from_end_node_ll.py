97e03ad75b95556656555d13a394ec5a5d57084e
Rajdeep Sodha <rajdeep.bsodha444@hotmail.com>
2025-11-09 17:03:44 +0530

    remove_n_last_node_ll

---
+++ strivers_sde_sheet/day_5_linked_list/remove_n_from_end_node_ll.py
@@ -0,0 +1,29 @@
+# Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        slow = fast = dummy
+        
+        for _ in range(n+1):
+            fast = fast.next
+
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
+        
+
+# Time complexity: O(N)
+# Space complexity: O(1)
